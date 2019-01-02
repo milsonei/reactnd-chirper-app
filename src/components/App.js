da771103dbb619add708ec19321e85f74bcfbe70
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-01-02 08:08:22 -0200

    Implementação final

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { handleInitialData } from '../actions/shared';
-import Dashbord from './Dahboard';
-import LoadingBar from 'react-redux-loading';
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashbord from './Dahboard'
+import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
+import Nav from './Nav'
 
 class App extends Component {
   componentDidMount(){
-    this.props.dispatch(handleInitialData());
+    this.props.dispatch(handleInitialData())
   }
   render() {
-   
+    
+      /**
+       * Normalmente seria criado uma div aqui, mas isto adicionaria outro div à DOM, o que não é recomendável.
+       * Então para resolver isso vamos usar Fragment, que permite passar um único filho para o componente router, de React Router, 
+       * mas não vai adicionar outros elementos à DOM
+       */
+       
     return (
-      <div>
-      <LoadingBar/>
-      { 
-        this.props.loading === true
-        ? null
-        : <Dashbord/>
-      }        
-      </div>
+      <Router>    
+        <Fragment>
+          <LoadingBar/>
+          <div className='container'>                
+            <Nav/>
+            { 
+              this.props.loading === true
+              ? null
+              : 
+              <div>
+                <Route path='/' exact component={Dashbord}/>
+                <Route path='/tweet/:id' exact component={TweetPage}/>
+                <Route path='/new' exact component={NewTweet}/>
+              </div>
+            }        
+          </div>
+        </Fragment>
+      </Router>
     )
   }
 }
